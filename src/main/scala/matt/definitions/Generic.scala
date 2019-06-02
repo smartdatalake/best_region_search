288eb7ee2040fa942587a8804ecff4729f44abe6
hamid <h.shahrivari@tue.nl>
2019-06-02 15:47:02 +0200

    Nround

--- src/main/scala/matt/definitions/Generic.scala
+++ src/main/scala/matt/definitions/Generic.scala
@@ -18,7 +18,6 @@ object Generic {
     return false
   }
 
-
   def poiToKeyValue(row: Row, width: Int, minmaxLong: (Double, Double), minmaxLat: (Double, Double), eps: Double
                     , geometryFactory: GeometryFactory, gridIndexer: GridIndexer): Seq[(Int, POI)] = {
     var gridSize = 0.0
@@ -27,15 +26,10 @@ object Generic {
     else
       gridSize = (minmaxLat._2 - minmaxLat._1) / width
     val cellSize = gridSize * eps
-
     val keywords = row.getAs[String]("keywords").split(",").toList;
     val long = row.getAs[Double]("longtitude")
     val lat = row.getAs[Double]("latitude")
-    //val newNode = extractNode(x.get(0), x.get(1), width, minmaxLong, minmaxLat);
     val result = ListBuffer[(Int, POI)]()
-   // result.add((1,new POI(row.getAs[String]("id"), row.getAs[String]("name")
-   //   , row.getAs[Double]("longtitude"), row.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
-   // return result
     return gridIndexer.getNodeIndex(row.getAs[Double]("longtitude"), row.getAs[Double]("latitude"))
       .map(x => (x._2 * width + x._1 + 1, new POI(row.getAs[String]("id"), row.getAs[String]("name")
         , row.getAs[Double]("longtitude"), row.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
@@ -46,34 +40,9 @@ object Generic {
   }
 
   def intersectsList(point: SpatialObject, list: ListBuffer[SpatialObject]): Boolean = {
-    for (point2 <- list) {
-      if (intersects(point, point2)) {
-       return true
-      }
-    }
+    for (point2 <- list)
+      if (intersects(point, point2))
+        return true
     return false
   }
 }
-/*    for(node <-1 until (width*width)+1 ){
-      if(IsInNodeRegion(long,lat,node,gridSize.asInstanceOf[Double],cellSize.asInstanceOf[Double],minmaxLong._1,minmaxLat._2,width)){
-
-      }
-     //   result.add((node, new POI(x.getAs[String]("id"), x.getAs[String]("name"), x.getAs[Double]("longtitude"), x.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
-   }
-    //
-   /* val regionI=((long - minmaxLong._1) / gridSize).toInt
-    val regionJ=((lat - minmaxLat._1) / gridSize).toInt
-    result.add((width*regionJ + regionI, new POI(x.getAs[String]("id"), x.getAs[String]("name"), x.getAs[Double]("longtitude"), x.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
-    if(long-(minmaxLong._1+regionI*gridSize)<cellSize&&(minmaxLat._1+(regionJ+1)*gridSize)-lat<cellSize&&regionI>0&&regionJ<width-1){
-      result.add((width*regionJ + (regionI-1), new POI(x.getAs[String]("id"), x.getAs[String]("name"), x.getAs[Double]("longtitude"), x.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
-      result.add((width*(regionJ+1) + regionI, new POI(x.getAs[String]("id"), x.getAs[String]("name"), x.getAs[Double]("longtitude"), x.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
-      result.add((width*(regionJ+1) + (regionI-1), new POI(x.getAs[String]("id"), x.getAs[String]("name"), x.getAs[Double]("longtitude"), x.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
-    }
-    else if(long-(minmaxLong._1+regionI*gridSize)<cellSize&&regionI>0){
-      result.add((width*regionJ + (regionI-1), new POI(x.getAs[String]("id"), x.getAs[String]("name"), x.getAs[Double]("longtitude"), x.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
-    }
-    else if((minmaxLat._1+(regionJ+1)*gridSize)-lat<cellSize&&regionJ<width-1){
-      result.add((width*(regionJ+1) + regionI, new POI(x.getAs[String]("id"), x.getAs[String]("name"), x.getAs[Double]("longtitude"), x.getAs[Double]("latitude"), keywords, 0, geometryFactory)))
-    }*/
-
-    result*/
--- src/main/scala/matt/distrib/OnestepAlgo.scala
+++ src/main/scala/matt/distrib/OnestepAlgo.scala
@@ -2,82 +2,35 @@
 package matt.distrib
 
 import org.apache.spark.rdd.RDD
-
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
-import matt.{DependencyGraph, POI, SpatialObject, SpatialObjectIdx}
-import matt.ca.{BCAIndexProgressiveDiv, BCAIndexProgressiveOneRound, UtilityScoreFunction}
+import matt.{ POI, SpatialObject}
+import matt.ca.BCAIndexProgressiveOneRound
 import matt.score.ScoreFunctionCount
 import matt.definitions.{Generic, GridIndexer}
-import matt.distrib.NstepAlgo.localAlgo
-
-import scala.util.control.Breaks.break
 
 object OnestepAlgo {
 
   def oneStepAlgo(input: (Int, Iterable[POI]), eps: Double, decayConstant: Double, topk: Int, gridIndexer: GridIndexer): List[SpatialObject] = {
-
     val pois: java.util.List[POI] = ListBuffer(input._2.toList: _*)
-    val scoreFunction = new ScoreFunctionCount[POI]();
-    val utilityScoreFunction = new UtilityScoreFunction();
-    val distinct = true
-    //val constRoundResult=Iterable[SpatialObject]
-    // Find the blocks.
-    val bcaFinder = new BCAIndexProgressiveOneRound(distinct, gridIndexer);
-    //while (con)
-
+    val scoreFunction = new ScoreFunctionCount[POI]()
+    val bcaFinder = new BCAIndexProgressiveOneRound(true, gridIndexer)
     bcaFinder.findBestCatchmentAreas(pois, eps, topk, scoreFunction).asInstanceOf[List[SpatialObject]]
-
   }
-
-  def buildIndex(bca: List[SpatialObject]): List[SpatialObjectIdx] = {
-
-    var finalIndex = ListBuffer[SpatialObjectIdx]();
-
-    var i = 0;
-    for (sp1 <- bca) {
-
-      // create new index entry
-      //var newSpatialObjectIndex = new matt.SpatialObjectIdx(i);
-
-      // find all overlapping rectangles with this rectangle.
-      var j = 0;
-      for (sp2 <- bca) {
-        if (i != j && Generic.intersects(sp1, sp2) && sp2.compareTo(sp1) < 0) {
-          // if the other rectangle has a lower score then insert it to our dependency list.
-          //   newSpatialObjectIndex.addDependency(j);
-        }
-        j = j + 1;
-      }
-
-      //   finalIndex += newSpatialObjectIndex;
-
-      i = i + 1;
-    }
-
-    finalIndex.toList
-  }
-
-  //  def buildGraph(x: Row
 
   def Run(nodeToPoint: RDD[(Int, POI)], eps: Double, decayConstant: Double, topk: Int, gridIndexer: GridIndexer) {
-
-    //
-    var Ans = ListBuffer[SpatialObject]();
-    val currentK = topk;
-    //nodeToPoint.collect().foreach(println)
-    val resultGroupedPerNode = nodeToPoint.groupByKey().flatMap(x => oneStepAlgo(x, eps, decayConstant, topk, gridIndexer));
-    val localAnswers = resultGroupedPerNode.collect().toList.sortBy(_.getScore).reverse
+    val Ans = ListBuffer[SpatialObject]()
+    val localAnswers = nodeToPoint.groupByKey().flatMap(x => oneStepAlgo(x, eps, decayConstant, topk, gridIndexer))
+      .collect().toList.sortBy(_.getScore).reverse
     println("***********************************************************************************************************************************")
     println(localAnswers.size)
-
     var pos = 0
-
     while (Ans.size < topk && pos != localAnswers.size) {
       if (!Generic.intersectsList(localAnswers.get(pos), Ans))
         Ans.add(localAnswers.get(pos))
       pos += 1
     }
+
     println("\n\n\n");
     println("Final Result");
     println("\n\n\n");
