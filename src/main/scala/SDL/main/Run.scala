535b3e2dd47159775722f8e1361de9890148f2eb
Hamid Shahrivari Joghan <h.shahrivari.joghan@tue.nl>
2021-02-10 17:44:52 +0100

    final version

--- src/main/scala/SDL/main/Run.scala
+++ src/main/scala/SDL/main/Run.scala
@@ -18,8 +18,8 @@ object Run {
   def main(args: Array[String]) {
     ///////Param & Config
     //////////////////////////
-    val pathToCSVs = "/home/hamid/"
-    val sparkServer = args(0)
+    // val pathToCSVs="/home/hamid/"
+    val sparkServer=args(0)
     val topk = args(1).toInt;
     val eps = args(2).toDouble
     val partitionsCNT = args(3).toInt
@@ -30,12 +30,13 @@ object Run {
     val keywordsColumn = args(8).toString
     val keywords = args(9).toString
     val input = args(10)
+    val dist = args(11).toBoolean
 
-    var keywords2 = ""
-    var keywordsColumn2 = ""
-    if (args.size == 13) {
-      keywordsColumn2 = args(11)
-      keywords2 = args(12)
+    var keywords2=""
+    var keywordsColumn2=""
+    if(args.size==14){
+      keywordsColumn2=args(12)
+      keywords2=args(13)
     }
     val csv_delimiter = "," //args(10)
     val keyword_delimiter = ";" //args(11)
@@ -43,7 +44,7 @@ object Run {
     Logger.getLogger("org").setLevel(Level.OFF)
     val spark = SparkSession
       .builder
-      .master(sparkServer)
+      //.master(sparkServer)
       .appName("BRS")
       .config("spark.dynamicAllocation.minExecutors", "25")
       .config("spark.dynamicAllocation.executorIdleTimeout", "50000s")
@@ -51,6 +52,7 @@ object Run {
       .config("spark.executor.heartbeatInterval", "100000s")
       .config("spark.worker.cleanup.enabled", "true")
       .getOrCreate()
+    import spark.implicits._
     spark.sparkContext.setLogLevel("WARN")
     val hadoopConfig: Configuration = spark.sparkContext.hadoopConfiguration
     hadoopConfig.set("fs.hdfs.impl", classOf[org.apache.hadoop.hdfs.DistributedFileSystem].getName)
@@ -60,10 +62,10 @@ object Run {
     System.err.println("Start;" + ";" + topk + ";" + eps + ";" + algo + ";" + Kprime + ";" + f + ";" + keywordsColumn + ";" + keywords + ";" + input)
     //////Read and split CSV coordination to (nodeNumber, POI) (assign poi to each worker)
     ///////////////////////////////////////////////////////////////
-    if (topk <= 0 || eps <= 0 || partitionsCNT <= 0 || algo > 2 || algo < 0 || base <= 0 || Kprime <= 0)
+    if (topk <= 0 || eps <= 0 || partitionsCNT <= 0 || algo > 9  || algo < 0 || base <= 0 || Kprime <= 0)
       throw new Exception("err : Invalid input ranges!!!")
     var inputData = spark.read.format("com.databricks.spark.csv").option("header", "true")
-      .option("inferSchema", "true").option("delimiter", csv_delimiter).option("nullValue", "null").load(pathToCSVs + input) //.sample(0.01)
+      .option("inferSchema", "true").option("delimiter", csv_delimiter).option("nullValue", "null").load(input) //.sample(0.01)
       .drop().filter(x => (x.getAs[Double]("lon") != null && x.getAs[Double]("lat") != null))
     //.filter(x => (x.getAs[Double]("lon") > 8.1 && x.getAs[Double]("lon") < 10.6)).filter(x => (x.getAs[Double]("lat") > 44.2 && x.getAs[Double]("lat") < 45.6)).sample(0.1,12345456)
     val keywordSplited = keywords.split(keyword_delimiter)
@@ -75,7 +77,6 @@ object Run {
         f
       })
     }
-    inputData.show()
 
     if (keywordsColumn2 != "") {
       inputData = inputData.filter(x => x.getAs[String](keywordsColumn2) != null && {
@@ -84,11 +85,10 @@ object Run {
         f
       })
     }
-    inputData.show()
-    inputData = if (f != "null") inputData.select("lon", "lat", f).filter(x => x.getAs(f) != null)
+    inputData = if (f != "null") inputData.select( "lon", "lat", f).filter(x => x.getAs(f) != null)
     else inputData.select("lon", "lat")
     if (inputData.count() == 0) {
-      println("{\n}")
+      println("[\n{\n}\n]")
       System.err.println("JobDone")
       spark.stop()
       return
@@ -97,19 +97,31 @@ object Run {
     val maxLong = inputData.select("lon").reduce((x, y) => if (x.getAs[Double]("lon") > y.getAs[Double]("lon")) x else y).getAs[Double](0)
     val minLat = inputData.select("lat").reduce((x, y) => if (x.getAs[Double]("lat") < y.getAs[Double]("lat")) x else y).getAs[Double](0)
     val maxLat = inputData.select("lat").reduce((x, y) => if (x.getAs[Double]("lat") > y.getAs[Double]("lat")) x else y).getAs[Double](0)
-    val minmaxLong = (minLong - eps / 100.0, maxLong + eps / 100.0);
-    val minmaxLat = (minLat - eps / 100.0, maxLat + eps / 100.0);
+    val minmaxLong = (minLong, maxLong );
+    val minmaxLat = (minLat, maxLat );
     // find to which node does each point belongs to : (NodeNo,Row)
     val width = math.sqrt(partitionsCNT).toInt
     val gridIndexer = new GridIndexer(width, eps, minmaxLong, minmaxLat)
     val geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
     ///////Start
     //////////////////////////////
+    if (algo == 9) { //MR
+      var result="["
+      val regions = inputData.select("lon","lat").map(coor=> (((coor.getDouble(0)-minmaxLong._1)/eps).toInt+","+((coor.getDouble(1)-minmaxLat._1)/eps).toInt,1))
+        .rdd.reduceByKey(_+_).sortBy(_._2,false).take(topk).toList
+      for(i<- 1 to topk){
+        result+=("\n{\n\"rank\":"+i+",\n\"center\":["+(regions(i-1)._1.split(",")(0).toInt*eps+eps/2.0+minmaxLong._1)
+          +","+(regions(i-1)._1.split(",")(1).toInt*eps+eps/2.0+minmaxLat._1)+"],\n\"score\":"+regions(i-1)._2+"\n},")
+      }
+      result=result.dropRight(1)
+      result+="\n]"
+      println(result)
+    }
     if (algo == 0) { //MR
       val nodeToPoint = inputData.rdd.flatMap(x => Generic.poiToKeyValue(x, geometryFactory, gridIndexer, f));
       nodeToPoint.persist(StorageLevel.MEMORY_AND_DISK);
       val t = System.nanoTime()
-      SDL.distrib.NstepAlgo.Run(nodeToPoint, eps, topk, Kprime, gridIndexer, base);
+      SDL.distrib.NstepAlgo.Run(nodeToPoint, eps, topk, Kprime, gridIndexer, base,dist);
       //println("Nstep:::       time:" + (System.nanoTime() - t) / 1000000000 + "s          eps:" + eps + "       topk:" + topk + "     partitionsCNT:" + partitionsCNT)
     }
     if (algo == 8) { //MR Approximate
@@ -117,11 +129,11 @@ object Run {
       nodeToPoint.persist(StorageLevel.MEMORY_AND_DISK);
       val t = System.nanoTime()
       //println("sigma 0.9")
-      SDL.distrib.NstepAlgoApp.Run(nodeToPoint, eps, topk, 0.9, gridIndexer);
+      SDL.distrib.NstepAlgoApp.Run(nodeToPoint, eps, topk, 0.9, gridIndexer,dist);
       //println("sigma 0.7")
-      SDL.distrib.NstepAlgoApp.Run(nodeToPoint, eps, topk, 0.7, gridIndexer);
+      SDL.distrib.NstepAlgoApp.Run(nodeToPoint, eps, topk, 0.7, gridIndexer,dist);
       //println("sigma 0.5")
-      SDL.distrib.NstepAlgoApp.Run(nodeToPoint, eps, topk, 0.5, gridIndexer);
+      SDL.distrib.NstepAlgoApp.Run(nodeToPoint, eps, topk, 0.5, gridIndexer,dist);
       // println("NstepApp:::       time:" + (System.nanoTime() - t) / 1000000000 + "s          eps:" + eps + "       topk:" + topk + "     partitionsCNT:" + partitionsCNT)
     }
     if (algo == 3) { //MR + region upper bound
