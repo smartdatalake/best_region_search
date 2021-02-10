03227996b441264412fcc22af7f0e87361379cc0
Hamid Shahrivari Joghan <h.shahrivari.joghan@tue.nl>
2021-02-10 17:01:38 +0100

    Added fat-jar configuration

--- src/main/scala/SDL/ca/BCAFinder.java
+++ src/main/scala/SDL/ca/BCAFinder.java
@@ -1,11 +1,11 @@
 package SDL.ca;
 
-import java.util.List;
-
 import SDL.SpatialObject;
 import SDL.score.ScoreFunction;
 
+import java.util.List;
+
 public abstract class BCAFinder<T extends SpatialObject> {
-	public abstract List<SpatialObject> findBestCatchmentAreas(List<T> pois, double eps, int topk,
-			ScoreFunction<T> scoreFunction);
+    public abstract List<SpatialObject> findBestCatchmentAreas(List<T> pois, double eps, int topk,
+                                                               ScoreFunction<T> scoreFunction);
 }
--- src/main/scala/SDL/ca/Rectangle.java
+++ src/main/scala/SDL/ca/Rectangle.java
@@ -5,29 +5,29 @@ import SDL.POI;
 import java.io.Serializable;
 
 public class Rectangle implements Comparable<Rectangle>, Serializable {
-	public float idx;
-	public boolean isMin;
-	public POI p;
+    public float idx;
+    public boolean isMin;
+    public POI p;
 
-	public Rectangle(float idx, boolean isMin, POI p) {
-		this.idx = idx;
-		this.isMin = isMin;
-		this.p = p;
-	}
+    public Rectangle(float idx, boolean isMin, POI p) {
+        this.idx = idx;
+        this.isMin = isMin;
+        this.p = p;
+    }
 
-	@Override
-	public int compareTo(Rectangle r) {
-		if (this.idx < r.idx)
-			return -1;
-		else if (this.idx == r.idx) {
-			if (!this.isMin && r.isMin) {
-				return -1;
-			} else if (this.isMin && !r.isMin) {
-				return 1;
-			} else
-				return 0;
-		} else {
-			return 1;
-		}
-	}
+    @Override
+    public int compareTo(Rectangle r) {
+        if (this.idx < r.idx)
+            return -1;
+        else if (this.idx == r.idx) {
+            if (!this.isMin && r.isMin) {
+                return -1;
+            } else if (this.isMin && !r.isMin) {
+                return 1;
+            } else
+                return 0;
+        } else {
+            return 1;
+        }
+    }
 }
